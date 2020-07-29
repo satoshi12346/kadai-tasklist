30776597f2b3a0deacee8fb773bd52e84d6bdc99
satoshi <sato1452369@gmail.com>
2020-07-29 12:03:41 +0000

    tasklist16

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   get 'sessions/new'
-  get 'users/new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "tasks#index"
   
   get 'login', to: 'sessions#new'
